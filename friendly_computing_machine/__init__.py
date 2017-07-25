484bc240d09fd834301d7abd2c07febf6a7980a5
cici <xiaohaiguicc@gmail.com>
2017-07-25 14:29:12 -0400

    Start the FCM

---
+++ friendly_computing_machine/__init__.py
@@ -0,0 +1,7 @@
+"""
+This is the base file of the friendly-computing-machine
+"""
+
+from . import math
+from .math import mult 
+from .math import add
---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,17 @@
+"""
+A samll set of functions for doing math operations.
+"""
+
+# Write a function named add that adds two values
+def add(arg1,arg2):
+    """
+    Function that adds two values
+    """
+    return 2 + 5
+def mult(arg1,arg2):
+    """
+    Function that multiple two values
+    """
+    return 2 * 5
+
+
